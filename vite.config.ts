0c8a0802e22feafe0a2767264cbb2d3fa4308ef4
tmg1981 <moradi6021@gmail.com>
2025-12-03 11:25:45 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
-  return {
-    plugins: [react()],
-    base: '/',
-    build: {
-      outDir: 'dist',
-    },
-    define: {
-      // Ensure it's always a string, falling back to empty string if undefined
-      'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
 })
